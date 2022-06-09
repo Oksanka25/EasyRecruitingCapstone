f0520e257586f7ac0cf76f04553366cbe1d3cf81
Oksanka25 <chekoksa25@gmail.com>
2022-06-09 19:14:02 -0400

    Added some styling to the login page

--- db/create_interviews.sql
+++ db/create_interviews.sql
@@ -1,6 +1,7 @@
 INSERT INTO main_app_interview(title, company, date, feedback, result, notes, client_id)
  VALUES 
- ('Web and Digital Coordinator', 'The Community Builders Inc', '05/20/22', 'Client felt pretty well on the interview', 'Client did get to the second round', 'Call the client on 5/25/22', 7 );INSERT INTO main_app_interview(title, company, date, feedback, result, notes, client_id)
+ ('Web and Digital Coordinator', 'The Community Builders Inc', '05/20/22', 'Client felt pretty well on the interview', 'Client did get to the second round', 'Call the client on 5/25/22', 7 );
+ INSERT INTO main_app_interview(title, company, date, feedback, result, notes, client_id)
  VALUES 
  ('Web and Digital Coordinator', 'The Community Builders Inc', '05/25/22', 'Client felt pretty well on the interview', 'Client did get to the second round', 'Call the client on 5/25/22 after 4pm', 7 );
 INSERT INTO main_app_interview(title, company, date, feedback, result, notes, client_id)
