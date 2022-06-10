8560ea4fba760d4aba81c584a0fd0162ea4d12dd
Oksanka25 <chekoksa25@gmail.com>
2022-06-10 14:15:10 -0400

    Config deployment

--- easy_recruiting/settings.py
+++ easy_recruiting/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_on_heroku
 import os
 from pathlib import Path
 
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/clients/'
 LOGOUT_REDIRECT_URL = '/'
+
+django_on_heroku.settings(locals())
